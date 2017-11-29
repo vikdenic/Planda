ced687dc9cc0c7430020b2002160847155c4ffaa
Vik Denic <vik.denic@gmail.com>
2017-11-28 18:59:40 -0600

    Initial Commit

---
+++ Planda/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Planda
+//
+//  Created by Vik Denic on 11/28/17.
+//  Copyright © 2017 nekturlabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
