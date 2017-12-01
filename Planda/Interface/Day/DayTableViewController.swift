331f889ee4108591eabb47b681740d2d19184c1a
Vik Denic <vik.denic@gmail.com>
2017-12-01 13:43:00 -0600

    build Day ui

--- Planda/Interface/Day/DayTableViewController.swift
+++ Planda/Interface/Day/DayTableViewController.swift
@@ -1,26 +1,24 @@
 //
-//  MonthTableViewController.swift
+//  DayTableViewController.swift
 //  Planda
 //
-//  Created by Vik Denic on 11/29/17.
+//  Created by Vik Denic on 12/1/17.
 //  Copyright © 2017 nekturlabs. All rights reserved.
 //
 
 import UIKit
 
-class MonthTableViewController: UITableViewController {
+class DayTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.navigationItem.title = Date().toMonthYearString()
     }
-    
+
     // MARK: - Table view data source
-    
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         switch section {
         case 0: return "Plan"
-        case 3: return "Review"
+        case 4: return "End of Day Reviw"
         default: return nil
         }
     }
